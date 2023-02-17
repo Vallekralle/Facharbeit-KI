18ca4596aec67f2970dc83a60908815ccc9c73d1
Valentin <kunzvalentin4@gmail.com>
2023-02-17 20:27:32 +0100

    Nun kann man mit einem Knopf die Zeichenflaeche zurücksetzen

--- Projekt 1/bloecke.py
+++ Projekt 1/bloecke.py
@@ -5,21 +5,21 @@ pygame.init()
 class Bloecke:
     SCHWARZ = (0, 0, 0)
     
-    def __init__(self, x, y, width, height, farbe):
+    def __init__(self, x, y, breite, hoehe, farbe):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
+        self.breite = breite
+        self.hoehe = hoehe
         self.farbe = farbe
         
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect = pygame.Rect(self.x, self.y, self.breite, self.hoehe)
         
         
-    def clicked(self, mousePos):
-        if (self.x <= mousePos[0] <= self.x + self.width and
-            self.y <= mousePos[1] <= self.y + self.height):
+    def gedrueckt(self, mousePos):
+        if (self.x <= mousePos[0] <= self.x + self.breite and
+            self.y <= mousePos[1] <= self.y + self.hoehe):
                 return True
         
         
     def zeichnen(self, win):
-        pygame.draw.rect(win, self.farbe, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(win, self.farbe, (self.x, self.y, self.breite, self.hoehe))
--- Projekt 1/main.py
+++ Projekt 1/main.py
@@ -1,26 +1,30 @@
 import pygame
 from bloecke import Bloecke
-from threading import Thread
 pygame.init()
 
 
 # Pygame Variablen zum einstellen des Fensters
 BREITE, HOEHE = 700, 700
 FENSTER = pygame.display.set_mode((BREITE, HOEHE))
-pygame.display.set_caption("KI zum Erkennen von handgeschriebenen Zahlen")
-
-FPS = 60
+pygame.display.set_caption(
+    "KI zum Erkennen von handgeschriebenen Zahlen")
+FPS = 120
 WEIß = (255, 255, 255)
-
 
 # Bildbreite für Raster und Modell festlegen
 bildBreite, bildHoehe = 300, 300
 
 
-# Attribute der Zeichenflaeche
-bloeckeAnzahl = 30 
+# Variablen der Zeichenflaeche
+bloeckeAnzahl = 50 
 rasterBloecke = []
 SCHWARZ = (0, 0, 0)
+
+# Variablen für die Knoepfe
+zurueckText = startText = ""
+zurueckKnf = startenKnf = object
+ROT = (255, 0, 0)
+
 
 def main():
     laufen = True
@@ -36,6 +40,8 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pressed = True
             if event.type == pygame.MOUSEBUTTONUP:
+                if zurueckKnf.gedrueckt(pygame.mouse.get_pos()):
+                    erzeugeZeichenFlaeche()
                 pressed = False
             if event.type == pygame.MOUSEMOTION and pressed == True:
                 malen(pygame.mouse.get_pos())
@@ -48,6 +54,11 @@ def zeichnen(pFenster):
     
     for bloeck in rasterBloecke:
         bloeck.zeichnen(pFenster)
+        
+    zurueckKnf.zeichnen(pFenster)
+    pFenster.blit(zurueckText, 
+                (zurueckKnf.x + (zurueckKnf.breite // 2 - zurueckText.get_width() // 2), 
+                zurueckKnf.y + (zurueckKnf.hoehe // 2 - zurueckText.get_height() // 2)))
     
     pygame.display.update()
     
@@ -56,17 +67,36 @@ def erzeugeZeichenFlaeche():
     breite = hoehe = bildBreite / bloeckeAnzahl
     x = y = BREITE / 2 - breite * bloeckeAnzahl / 2
     
+    erzeugeZurueckKnf(y)
+    
     for _ in range(bloeckeAnzahl):
         for _ in range(bloeckeAnzahl):
-            rasterBloecke.append(Bloecke(x, y, breite, hoehe, (0, 255, 0)))
+            rasterBloecke.append(Bloecke(x, y, breite, hoehe, 
+                                         (240, 240, 240)))
             x += breite
         y += hoehe
-        x = BREITE // 2 - breite * bloeckeAnzahl // 2
+        x = x - breite * bloeckeAnzahl
     
+
+def erzeugeZurueckKnf(pY):
+    global zurueckText
+    global zurueckKnf
+    
+    # Der Text für den Knopf
+    font = pygame.font.SysFont("monospace", 25)
+    zurueckText = font.render("zurück", 1, (0, 0, 0))
+    
+    # Der Knopf
+    x = BREITE // 2 - zurueckText.get_width() // 2
+    zurueckKnf = Bloecke(x, pY - zurueckText.get_height() * 2, 
+                         zurueckText.get_width() + 10, 
+                         zurueckText.get_height() + 10, 
+                         ROT)
+
 
 def malen(mousePos):
     for bloeck in rasterBloecke:
-        if bloeck.clicked(mousePos):
+        if bloeck.gedrueckt(mousePos):
             bloeck.farbe = SCHWARZ
 
 
